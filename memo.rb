950b563106f34546794c9e26f6eacd734dd0ebab
EC2 Default User <ec2-user@ip-172-31-57-140.ec2.internal>
2021-06-03 09:57:43 +0000

    Memo

--- memo.rb
+++ memo.rb
@@ -1,25 +1,28 @@
 require "csv"
-puts "1(新規でメモを作成) 2(既存のメモ編集する)"
-memo_type = gets.chomp.to_s
-if memo_type ="1"
-  puts "拡張子を除いたファイルを入力してください。" 
+while true
+puts "1(新規でメモを作成) 2(既存のメモ編集する) 3(終了する)"
+memo_type = gets.to_i
+if memo_type == 1
+  puts "拡張子を除いたファイル名を入力してください。" 
   file_name = gets.chomp
   puts "メモしたい内容を入力してください。"
-  puts "完了したらCtrl＋Dを押下してください。"
   memo = gets.chomp
-
+  puts "もし終了するなら3を入力してください。"
   CSV.open("#{file_name}.csv","w") do |write|
     write << ["#{memo}"]
   end
-elsif memo_type ="2"
+elsif memo_type == 2
   puts "拡張子を除いたファイル名を入力してください。"
   file_name = gets.chomp
   puts "追記したい内容を入力してください。"
-  puts "完了したらCtrl＋Dを押下してください。"
   memo = gets.chomp
+  puts "もし終了するなら3を入力してください。"
   CSV.open("#{file_name}.csv","a") do |write|
     write << ["#{memo}"]
   end
-else
-  puts "1または2を入力してください。"
+elsif memo_type == 3
+  break
+else 
+  puts "1または2または3を入力してください。"
+end
 end
